60b59d8dbfa394586f41da1f2e83e041979e9ea1
tomaskreuzing <tomas.kreuzing@gmail.com>
2024-11-17 16:58:26 +0000

    winsorizer created and tested (3/3 tests passed)

--- ps3/preprocessing/_winsorizer.py
+++ ps3/preprocessing/_winsorizer.py
@@ -5,11 +5,21 @@ from sklearn.utils.validation import check_is_fitted
 # TODO: Write a simple Winsorizer transformer which takes a lower and upper quantile and cuts the
 # data accordingly
 class Winsorizer(BaseEstimator, TransformerMixin):
-    def __init__(self):
-        pass
+    def __init__(self, lower_quantile, upper_quantile):
+        self.lower_quantile = lower_quantile
+        self.upper_quantile = upper_quantile
 
     def fit(self, X, y=None):
-        pass
+        X = np.asarray(X)
+        self.lower_quantile_ = np.quantile(X, self.lower_quantile)
+        self.upper_quantile_ = np.quantile(X, self.upper_quantile)
+        return self
+
 
     def transform(self, X):
-        pass
+        X = np.clip(X, a_min=self.lower_quantile_, a_max= self.upper_quantile_)
+        return X
+    
+    def fit_transform(self, X):
+        self.fit(X)
+        return self.transform(X)
--- tests/preprocessing/test_winsorizer.py
+++ tests/preprocessing/test_winsorizer.py
@@ -10,5 +10,14 @@ from ps3.preprocessing import Winsorizer
 def test_winsorizer(lower_quantile, upper_quantile):
 
     X = np.random.normal(0, 1, 1000)
+    winsorizer = Winsorizer(lower_quantile=lower_quantile, upper_quantile=upper_quantile)
+    transformed = winsorizer.fit_transform(X)
 
-    assert False
+    lower_bound = np.quantile(X, lower_quantile)
+    upper_bound = np.quantile(X, upper_quantile)
+
+    assert X.size > 0, "Input array X must not be empty"
+    assert len(transformed) == len(X), "Output length does not match input length"
+
+    assert np.all(transformed >= lower_bound), f"Values below {lower_bound} found"
+    assert np.all(transformed <= upper_bound), f"Values above {upper_bound} found"
